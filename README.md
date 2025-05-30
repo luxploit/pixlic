c7f02d1dabdeaa09d2be4020e7907b97e41f43b6
Laura Ava Streisky <laura@luxploit.net>
2025-05-30 12:36:55 +0200

    add untested support for PIX 501, rework arguments and bring bruteforce support to the master branch

--- README.md
+++ README.md
@@ -1,20 +1,25 @@
 # [luxploit](https://luxploit.net)/[pixlic](https://github.com/luxploit/pixlic)
 
-A simple keygen for generating unrestricted licenses (UR), including activation for 3DES and AES Encryption for VPNs, for Cisco PIX 515, 515e and 525 and 535 firewall appliances.
+A simple keygen for generating unrestricted licenses (UR), including activation for 3DES and AES Encryption for VPNs, for Cisco PIX 500 series firewall appliances.
 
 ## Installation
 
 Either clone this repo and run `./build.sh` in Git Bash for Windows or your local \*nix shell or download a pre-compiled version found in the releases tab [here](https://github.com/luxploit/pixlic/releases)
 
-## Usage
+## Example Usage
+
+**Run `./pixlic.lxb -list` to see all available PIX models**
+
+Example for PIX 515e Router:
 
 ```
-./pixlic.lxb [serial number] [p515|p525]
+./pixlic.lxb -serial "809112952" -model "PIX 515"
 ```
-
-_Note: for PIX 535 appliances, use the `p515` setting_
 
 ## Credits
 
-- https://blog.bjdch.org/2015/01/1976
-- http://www.anticisco.ru/forum/viewtopic.php?f=2&t=920
+- Algorithm Leak: [AntiCisco poster "Pinguin"](http://www.anticisco.ru/forum/viewtopic.php?f=2&t=920)
+- Initial find that led to this project being possible: [Dominic's blog about the AntiCisco thread](https://blog.bjdch.org/2015/01/1976)
+- PIX 525 UR Serial and Key for testing: [RuTracker's Massive PIX/ASA collection](https://rutracker.org/forum/viewtopic.php?t=831309)
+- PIX 501 Future and more goodies: @\_stargo\_ over on the Serial Port discord
+- Inspiration that led to this project: [ClabRetro's \"PIX Firewall Failover\" video](https://www.youtube.com/watch?v=719c5ed_ogc)
--- main.go
+++ main.go
@@ -3,11 +3,11 @@ package main
 import (
 	"crypto/md5"
 	"encoding/hex"
+	"flag"
 	"fmt"
 	"os"
 	"strconv"
-
-	"github.com/spf13/cast"
+	"strings"
 )
 
 func invertHexBytes(hexStr string) string {
@@ -24,43 +24,116 @@ const (
 	PIX_525_UR = "38040000"
 )
 
+var licenseFutures = map[string]string{
+	"PIX 501": "2A800100", // needs testing
+	"PIX 506": "????????",
+	"PIX 515": "39000000",
+	"PIX 520": "????????",
+	"PIX 525": "38040000",
+	"PIX 535": "39000000", //needs testing
+}
+
 func main() {
 
-	if len(os.Args) < 3 {
-		fmt.Println("Usage: ./pixlic <serial number> [p525|p515]")
+	cliSerial := flag.Int64("serial", 0, "Specify a PIX serial number")
+	cliModel := flag.String("model", "PIX 515", "Specify a PIX Model or set \"dev\" for development")
+	cliListModels := flag.Bool("list", false, "List all available PIX models")
+
+	devLicFuture := flag.String("dev-future", "", "(Test) Pass custom license future value")
+	devPkey := flag.String("dev-pkey", "", "(Test) Pass a license of an existing UR box to match against")
+	devBruteForce := flag.Bool("dev-bruteforce", false, "(Test) Bruteforce a future value based on license and SN")
+
+	if len(os.Args) < 2 {
+		flag.Usage()
+	}
+
+	if *cliListModels {
+		println("Supported PIX Models:")
+		println("  - PIX 515 (Includes 515e)")
+		println("  - PIX 525")
+		println("\nUntested Models:")
+		println("  - PIX 501")
+		println("  - PIX 535")
+		println("\nUnsupported Models:")
+		println("  - PIX 506 (Includes 506e)")
+		println("  - PIX 520")
 		os.Exit(1)
 	}
 
-	arg, err := cast.ToInt64E(os.Args[1])
-	if err != nil {
-		fmt.Println("Invalid serial number provided!")
-		fmt.Println("Usage: ./pixlic <serial number> [p525|p515]")
-		os.Exit(1)
+	arg := *cliSerial
+	if arg == 0 {
+		println("Invalid serial number provided!")
+		flag.Usage()
 	}
 
-	pixMode := os.Args[2]
-	license := ""
-	if pixMode == "p515" {
-		license = PIX_515_UR
-	} else if pixMode == "p525" {
-		license = PIX_525_UR
-	} else {
-		fmt.Println("Invalid pix mode provided!")
-		fmt.Println("Usage: ./pixlic <serial number> [p525|p515]")
-		os.Exit(1)
+	pixModel := *cliModel
+	future, ok := licenseFutures[pixModel]
+	if !ok && pixModel != "dev" {
+		println("Invalid pix model provided!")
+		flag.Usage()
+	}
+
+	if pixModel == "dev" && *devLicFuture != "" {
+		future = *devLicFuture
 	}
 
 	serial := invertHexBytes(strconv.FormatInt(arg, 16))
-	data, err := hex.DecodeString(license + serial)
-	if err != nil {
-		panic(err)
-	}
-	hash := fmt.Sprintf("%x", md5.Sum(data))
 
-	print("Here's your PIX UR License: ")
-	for idx := 0; idx < 16; idx += 4 {
-		part := hash[(idx * 2):((idx + 4) * 2)]
-		fmt.Printf("0x%s ", invertHexBytes(part))
+	if *devBruteForce {
+		bruteForceGenerate(pixModel, serial, *devPkey)
+	} else {
+		data, err := hex.DecodeString(future + serial)
+		if err != nil {
+			panic(err)
+		}
+
+		hash := fmt.Sprintf("%x", md5.Sum(data))
+		generateOneTime(pixModel, future, hash, *devPkey)
 	}
-	println()
+
+}
+
+func generateOneTime(pixModel, future, hash, devKey string) {
+	pkey := ""
+	print("Here's your PIX (UR) License: ")
+	for idx := 0; idx < 16; idx += 4 {
+		part := invertHexBytes(hash[(idx * 2):((idx + 4) * 2)])
+		pkey += "0x" + part
+	}
+	println(pkey)
+
+	pkey = strings.TrimRight(pkey, " ")
+	if devKey != "" && devKey == pkey {
+		println("DEBUG: LICENSE KEY MATCH!")
+		fmt.Printf("pkey: %s", pkey)
+		fmt.Printf("devKey: %s", devKey)
+		fmt.Printf("future: %s", future)
+		fmt.Printf("model: %s", pixModel)
+	}
+}
+
+func bruteForceGenerate(pixModel, serial, devKey string) {
+	for idx := range 0x5F5E0FF {
+		genFuture := invertHexBytes(fmt.Sprintf("%08X", idx))
+		data, err := hex.DecodeString(genFuture + serial)
+		if err != nil {
+			continue
+		}
+
+		hash := fmt.Sprintf("%x", md5.Sum(data))
+		pkey := ""
+		for idx := 0; idx < 16; idx += 4 {
+			part := invertHexBytes(hash[(idx * 2):((idx + 4) * 2)])
+			pkey += "0x" + part
+		}
+		pkey = strings.TrimRight(pkey, " ")
+
+		if devKey != "" && devKey == pkey {
+			println("DEBUG: MAGIC FUTURE FOUND!")
+			fmt.Printf("future: %s", genFuture)
+			fmt.Printf("pkey: %s", pkey)
+			fmt.Printf("devKey: %s", devKey)
+			fmt.Printf("model: %s", pixModel)
+		}
+	}
 }
